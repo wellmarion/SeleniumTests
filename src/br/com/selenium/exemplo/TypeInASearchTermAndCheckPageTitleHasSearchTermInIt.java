ed916d62b60a05c6784298e8ea5bb6212f6cdffa
Wellington Marion <wellmarion@gmail.com>
2012-02-08 08:39:28 -0200

    Criando novos testes

--- src/br/com/selenium/exemplo/TypeInASearchTermAndCheckPageTitleHasSearchTermInIt.java
+++ src/br/com/selenium/exemplo/TypeInASearchTermAndCheckPageTitleHasSearchTermInIt.java
@@ -4,19 +4,16 @@ import org.junit.Test;
 
 import br.com.selenium.utils.InitialiseSeleniumRCServer;
 
-public class TypeInSearchTerm extends InitialiseSeleniumRCServer {
+public class TypeInASearchTermAndCheckPageTitleHasSearchTermInIt extends InitialiseSeleniumRCServer {
 
 	@Test
-	public void typeInASearchTerm() {
-		// Selenium selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://www.compendiumdev.co.uk");
-		// selenium.start();
+	public void typeInASearchTermAndCheckPageTitleHasSearchTermInIt() {
 		selenium.open("http://www.compendiumdev.co.uk/selenium/search.php");
 		selenium.type("xpath=//input[@name='q']", "Selenium-RC");
 		selenium.click("xpath=//input[@name='btnG' and @type='submit']");
 		selenium.waitForPageToLoad("30000");
-		// selenium.close();
-		// selenium.stop();
-
+		String pageTitle = selenium.getTitle();
+		assertTrue("Page Title does not contain Selenium-RC search term: " + pageTitle, pageTitle.contains("Selenium-RC"));
 	}
 
 }
